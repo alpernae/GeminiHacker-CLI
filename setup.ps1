d298fcae59df0421c35c85cffe0848a60565269c
Alperen <alperene@aof.anadolu.edu.tr>
2024-03-24 15:17:49 +0300

    Delete setup.ps1

--- setup.ps1
+++
@@ -1,9 +0,0 @@
-# Directory containing the geminihacker script
-$scriptDir = "C:\path\to"
-
-# Add geminihacker directory to PATH
-$currentPath = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
-$newPath = $currentPath + ";$scriptDir"
-[System.Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
-
-Write-Host "Geminihacker is now globally accessible on Windows."
